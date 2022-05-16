1639624db9f654554b6c81ea73f1cafb620fa70b
puja <pujarana1796@gmail.com>
2022-05-16 20:59:33 +0530

    puja added first commit

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("welcome to User Registration Problems");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace UserRegistration
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem Using REGEX!");
+
+            validateName userRegistration = new validateName();
+            userRegistration.FirstName();
+        }
+    }
+}
